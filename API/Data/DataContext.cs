025d8529015526701681bae78713b29e2ae8b7b5
Aaron A. Brown <kilonow1275@hotmail.com>
2025-10-02 21:23:59 -0600

    First commit

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    // video 009 Adding a DbContext
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+
+    }
+}
